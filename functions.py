0eb28127f7884cb832ce4540fcc999a748af9cf3
sagnikkvs-glitch <sagnik.kvs@gmail.com>
2026-07-29 22:05:17 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,18 @@
+# What are functions?
+# Functions are blocks of code that perform a specific task.
+
+def function_name(name):
+    print("Hello", name)
+
+function_name("Sagnik")
+
+
+def cal():
+    a = int(input("Please enter the first number: "))
+    b = int(input("Please enter the second number: "))
+    return a + b
+
+result = cal()
+print("The sum is:", result)
+
+
